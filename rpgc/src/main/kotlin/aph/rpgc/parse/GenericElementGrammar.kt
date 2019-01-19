865d9cefc3851de9b5911bd7f97359bfdd8f6318
Aaron Harris <aaron.patrick.harris@gmail.com>
2019-01-19 11:00:00 -0600

    Extend generic element grammar to handle single-paragraph description

--- rpgc/src/main/kotlin/aph/rpgc/parse/GenericElementGrammar.kt
+++ rpgc/src/main/kotlin/aph/rpgc/parse/GenericElementGrammar.kt
@@ -2,8 +2,11 @@ package aph.rpgc.parse
 
 import aph.rpgc.core.GenericElement
 import aph.rpgc.core.Label
+import aph.rpgc.core.Paragraph
 import com.github.h0tk3y.betterParse.combinators.and
 import com.github.h0tk3y.betterParse.combinators.map
+import com.github.h0tk3y.betterParse.combinators.optional
+import com.github.h0tk3y.betterParse.combinators.separatedTerms
 import com.github.h0tk3y.betterParse.combinators.skip
 import com.github.h0tk3y.betterParse.combinators.use
 import com.github.h0tk3y.betterParse.grammar.Grammar
@@ -12,11 +15,18 @@ import com.github.h0tk3y.betterParse.parser.Parser
 object GenericElementGrammar : Grammar<GenericElement>() {
 
     private val nameStartMarker by token("# ")
-    private val name by token(".+")
 
-    private val nameParser: Parser<Label> by skip(nameStartMarker) and name use { text }
+    private val line by token(".+")
+    private val newline by token("\n")
 
-    override val rootParser: Parser<GenericElement> by nameParser map {
-        GenericElement(name = it)
+    private val paragraphParser: Parser<Paragraph> by separatedTerms(line, newline) use {
+        joinToString(separator = " ") { it.text }
+    }
+
+    private val nameParser: Parser<Label> by skip(nameStartMarker) and line and skip(optional(newline)) use { text }
+    private val descriptionParser: Parser<List<Paragraph>> by optional(paragraphParser) map { listOfNotNull(it) }
+
+    override val rootParser: Parser<GenericElement> by nameParser and descriptionParser use {
+        GenericElement(name = t1, description = t2)
     }
 }
--- rpgc/src/test/kotlin/aph/rpgc/parse/GenericElementGrammarTests.kt
+++ rpgc/src/test/kotlin/aph/rpgc/parse/GenericElementGrammarTests.kt
@@ -2,6 +2,7 @@ package aph.rpgc.parse
 
 import aph.rpgc.core.GenericElement
 import aph.rpgc.jqwik.Line
+import aph.rpgc.jqwik.Paragraph
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import net.jqwik.api.ForAll
 import net.jqwik.api.Property
@@ -13,6 +14,19 @@ class GenericElementGrammarTests {
         assertEquals(
             GenericElement(name = elementName),
             GenericElementGrammar.parseToEnd("# $elementName")
+        )
+    }
+
+    @Property fun `Can parse element with one paragraph description`(
+        @ForAll @Line elementName: String,
+        @ForAll @Paragraph description: String
+    ) {
+        assertEquals(
+            GenericElement(name = elementName, description = listOf(description.replace('\n', ' '))),
+            GenericElementGrammar.parseToEnd("""
+                |# $elementName
+                ${description.prependIndent("|")}
+            """.trimMargin())
         )
     }
 }
